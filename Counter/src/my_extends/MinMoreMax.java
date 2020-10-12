81273f67fc38ae2d0e7a8f0fa4b5676d1e3ef907
Working <Working>
2020-10-12 21:58:24 +0300

    Создано исключение: значение минимума не может превосходить значение максимума

---
+++ Counter/src/my_extends/MinMoreMax.java
@@ -0,0 +1,13 @@
+package my_extends;
+
+public class MinMoreMax extends Exception{
+    String s = "Минимальное значение не может превышать максимальное ";
+    public MinMoreMax(){};
+    public MinMoreMax(int min, int max){s += "("+min+">"+max+")";}
+    @Override
+    public String getMessage(){ return s;}
+    @Override
+    public String toString() {
+        return s;
+    }
+}
